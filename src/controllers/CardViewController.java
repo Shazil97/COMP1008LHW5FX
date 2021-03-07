b32fa0a855118bc2d1125ee0960dfe81d53ee49a
Shazil <sjamal@lakeheadu.ca>
2021-03-07 13:19:38 -0500

    Scene builder

--- src/controllers/CardViewController.java
+++ src/controllers/CardViewController.java
@@ -1,5 +1,16 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+
 public class CardViewController {
+    @FXML
+    private Label facenamelabel;
+
+    @FXML
+    private Label suitlabel;
+
 
 }
