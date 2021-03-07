bceaa5563e2f2ebc96d327e01843bac296025d7b
Shazil <sjamal@lakeheadu.ca>
2021-03-07 04:04:10 -0500

    Calling Abstrat method completed

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 
 import static javafx.application.Application.launch;
@@ -14,6 +16,8 @@ public class Main extends Application {
      * This method was abstract in the Application class. This
      * is where we define what we want the application to do when
      * it starts.
+     * In our case, we want our application to load the fxml file called
+     * cardView.fxml
      * @param stage
      * @throws Exception
      */
@@ -21,6 +25,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("views/cardView.fxml"));
 
     }
 }
