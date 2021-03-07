cfdab70f34eedbca561d7f059637f4e4dd7ec6f0
Shazil <sjamal@lakeheadu.ca>
2021-03-07 03:58:21 -0500

    Calling Abstrat method completed

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
+    /**
+     * This method was abstract in the Application class. This
+     * is where we define what we want the application to do when
+     * it starts.
+     * @param stage
+     * @throws Exception
+     */
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
