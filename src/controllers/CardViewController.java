2e6e52319998e5a5f385159c33057d4457631da6
Shazil <sjamal@lakeheadu.ca>
2021-03-07 13:24:03 -0500

    Scene builder

--- src/controllers/CardViewController.java
+++ src/controllers/CardViewController.java
@@ -1,16 +1,24 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class CardViewController {
+public class CardViewController implements Initializable {
     @FXML
     private Label facenamelabel;
+
 
     @FXML
     private Label suitlabel;
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
